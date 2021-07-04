1a27b42f7a7ffc67a93df5bfb7ef653990a5e197
jalnor <norrisj0612@gmail.com>
2021-07-04 11:50:09 -0400

    Initialized project and pushing to github. closes #1

---
+++ src/main/java/com/nordico/retailwebsite/RetailWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.nordico.retailwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailWebsiteApplication.class, args);
+    }
+
+}
